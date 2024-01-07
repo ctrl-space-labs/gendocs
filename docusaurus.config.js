68b2e8676fbb9b82a0c703e20e9975d5dea46047
sekasx <sekas.x@gmail.com>
2024-01-07 22:59:20 +0200

    Fix GitHub Pages configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/g.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ctrl-space-labs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/gendocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
